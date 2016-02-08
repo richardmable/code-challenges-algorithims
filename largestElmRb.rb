59cd65f96c4092fee20d515b1b5e700f185bd6cc
Richard Mable <richardbmable@gmail.com>
2016-02-08 16:22:53 -0500

    reverse of array written

--- largestElmRb.rb
+++ largestElmRb.rb
@@ -1,1 +1,42 @@
 # Write a function that returns the largest element in a list.
+
+list = [-1, -50, -40, 123, 452, 346, 785, 114, 675, 32, 567, 231, 562, 743, 764, 247, 535, 742, 735]
+counter = 0
+
+#compare each num to the next
+#if larger keep if smaller discard
+#would have to go through entire list
+
+# every single number compares to the one to the left
+#the bigger one gets kept
+#repeat
+
+def largest_elm(list)
+	list.each do |x|
+		
+end
+
+largest_elm(list)
+
+
+
+
+	# listLength = list.length - 1
+	# counter = 0
+	# list.each do |x|
+	# 	if counter >= listLength
+	# 		break
+	# 	elsif x[counter + 2]
+	# 		if x[counter] < x[counter + 1] > x[counter + 2]
+	# 		puts x[counter + 1]
+	# 		end
+	# 	elsif x[counter + 2]
+	# 		if x[counter] < x[counter + 2]
+	# 		puts x[counter + 2]
+	# 		end
+	# 	else
+	# 		puts x[counter]
+	# 	end
+	# 	counter = counter + 1
+	# end
+
---
+++ reverse_list.rb
@@ -0,0 +1,52 @@
+#Write function that reverses a list, preferably in place.
+list = [-1, -50, -40, 123, 452, 346, 785, 114, 675, 32, 567, 231, 562, 743, 764, 247, 535, 742, 735]
+
+# method that takes in array
+def reverse_array(array)
+# the length of the list is the length of the array - 1	
+	listLength = array.length - 1
+	# set a new empty array
+	listReverse = []
+	# go through each index of original array and push it into new array
+	#.push will insert each item onto end of new array
+	#start with taking last index of original array
+	for i in 0..listLength
+		listReverse.push(array[listLength - i])
+	end
+	#print the result
+	puts listReverse.inspect
+end
+
+#swaps postions of any two values in arrays
+def swap(valueOne, valueTwo)
+	#hold the value of valueOne
+	 hold = valueOne
+	 #set valueOne to valueTwo
+	 valueOne = valueTwo
+	 #set valueTwo to valueOne
+	 valueTwo = hold
+	 #return the swapped values
+	 return valueOne, valueTwo
+end
+
+# method that takes in array
+def reverse_array_better(array)
+# the length of the list is the length of the array - 1	
+	listLength = array.length / 2
+	# go through each index of the array and push it into begining of array using .insert
+	#start with taking last index of original array
+	for i in 0..listLength
+		swap(i, listLength - i)
+
+
+	end
+
+end
+
+puts "original:"
+puts list.inspect
+puts "reverse:"
+reverse_array(list)
+puts ""
+puts ""
+reverse_array_better(list)
